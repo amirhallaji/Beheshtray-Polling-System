52ea664f4db3980ed12f0dcdaa16cd8b6b370105
Mohammad Khoddam <mkh2097@gmail.com>
2021-05-28 17:00:10 +0430

    Added new XML!

---
+++ main/java/ir/sample/app/BeheshtRay/views/TeacherComment2.java
@@ -0,0 +1,11 @@
+package ir.sample.app.BeheshtRay.views;
+
+import ir.appsan.sdk.View;
+
+public class TeacherComment2 extends View {
+
+    public TeacherComment2() {
+        super();
+        setViewPath("TeacherComment2.xml");
+    }
+}
