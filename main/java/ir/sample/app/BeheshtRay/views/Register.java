281f4ccb2e4ce9c517cd5c9744b2f4f189e3b766
armanhmi <armanhm@mail.com>
2021-05-29 16:59:40 +0430

    Register page added

---
+++ main/java/ir/sample/app/BeheshtRay/views/Register.java
@@ -0,0 +1,11 @@
+package ir.sample.app.BeheshtRay.views;
+
+        import ir.appsan.sdk.View;
+
+public class Register extends View {
+
+    public Register() {
+        super();
+        setViewPath("Register.xml");
+    }
+}
